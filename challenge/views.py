5c9bf48c5fc9912226f7bd2bdd3aa383ac795a0e
jiinkyung <jinkyung010310@gmail.com>
2022-08-26 01:20:12 +0900

    챌린지 상세페이지 연결

--- challenge/views.py
+++ challenge/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ChallengeTag, Professor, Company, Senior
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 
 def challenge(request):
@@ -9,6 +10,17 @@ def challenge(request):
     tags = ChallengeTag.objects.all()
     return render(request, 'challenge.html', {'p_posts':p_posts, 'c_posts':c_posts, 's_posts':s_posts, 'tags':tags})
     
+def p_detail(request, post_id):
+    post = get_object_or_404(Professor, pk=post_id)
+    tags = ChallengeTag.objects.all()
+    return render(request, 'challengeDetail.html', {'post':post, 'tags':tags})
 
+def c_detail(request, post_id):
+    post = get_object_or_404(Company, pk=post_id)
+    tags = ChallengeTag.objects.all()
+    return render(request, 'challengeDetail.html', {'post':post, 'tags':tags})
 
-    
+def s_detail(request, post_id):
+    post = get_object_or_404(Senior, pk=post_id)
+    tags = ChallengeTag.objects.all()
+    return render(request, 'challengeDetail.html', {'post':post, 'tags':tags})
